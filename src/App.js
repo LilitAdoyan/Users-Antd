1119b4d79e2f22d1f9fcbe6ef4fa7014c116a61b
lilit_ad <lilith.adoyan@gmail.com>
2022-11-09 01:45:49 +0400

    fetched user data on the first rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
+
 import './App.css';
+import {Table} from 'antd';
+
+const columns=[
+    {
+      title:'Avatar',
+      dataIndex:'avatar_url',
+      render: theImageURL => <img alt={theImageURL} src={theImageURL} width={50}/>  // 'theImageURL' is the variable you must declare in order the render the URL
+    },
+    {
+      title:'Username',
+      dataIndex:'login'
+    },
+    {
+      title:'Type',
+      dataIndex:'type'
+    },
+    {
+      title:'Options',
+      dataIndex:''
+    }
+  ]
 
 function App() {
+  const [dataSource, setDataSource]=useState([])
+  const [totalPages,setTotalPages]=useState(1)
+  const [loading,setLoading]=useState(false)
+  
+  useEffect(()=>{fetchUsers()},[])
+
+  const fetchUsers=()=>{
+    setLoading(true)
+    fetch('https://api.github.com/users').then((res)=>{res.json().then((response)=>{
+      console.log(response)
+      setDataSource(response)
+      setTotalPages(response.length/10)
+      setLoading(false)
+    })})
+  }  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Table 
+     loading={loading}
+     columns={columns}
+     dataSource={dataSource}
+     ></Table>
     </div>
   );
 }
